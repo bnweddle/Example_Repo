290aa0f400994e01a75cce4bc643d4f1950d3b9c
Bethany Weddle <bweddle@niar.wichita.edu>
2022-06-10 16:19:37 -0500

    Add project files.

---
+++ Example_Console_App/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(1000);
+
+            string thisIsaString = "Ta Da";
+            int countDown = 10;
+
+            Console.WriteLine("Count Down" + thisIsaString);
+            for (int i = 1; i <= countDown; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
